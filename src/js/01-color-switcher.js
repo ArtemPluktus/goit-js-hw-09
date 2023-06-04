2f0aabb13f97f413420fcc8e83abdcbef48157eb
ArtemPluktus <artempluktus@gmail.com>
2023-06-04 13:41:32 +0200

    1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const start = document.querySelector(`[data-start]`);
+
+const stop = document.querySelector(`[data-stop]`);
+
+const body = document.querySelector(`body`);
+
+start.addEventListener(`click`, () => {
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+});
+
+stop.addEventListener(`click`, () => {
+    clearInterval(intervalId);
+});
